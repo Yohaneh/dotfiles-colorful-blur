f44a3205a8deb5a01ceb9e42a32a33e546bc56d4
Yohaneh <90387805+Yohaneh@users.noreply.github.com>
2026-01-04 22:10:09 -0300

    Delete .config/waybar directory

--- .config/waybar/player.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-tooltip="$(playerctl metadata title) - $(playerctl metadata artist) - $(playerctl metadata album)"
-text="$(playerctl metadata title) - $(playerctl metadata artist)"
-
-echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\"}"
